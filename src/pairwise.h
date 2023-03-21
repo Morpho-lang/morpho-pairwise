64dca53bf687880186dae40b0ff7ab9087269db2
Chaitanya Joshi <chaitanya.joshi@tufts.edu>
2023-03-21 16:00:46 -0400

    First code for LJ and periodicity
    This commit adds the first code for the Lennard Jones pairwise potential. In addition, it sets up an initial framework for periodic boundary conditions, with a cubic box currently implemented.

--- src/pairwise.h
+++ src/pairwise.h
@@ -6,15 +6,20 @@
 
 #define PAIRWISE_POTENTIAL_PROPERTY          "potential"
 #define PAIRWISE_CUTOFF_PROPERTY             "cutoff"
+#define PAIRWISE_PERIODIC_PROPERTY           "box"
 #define PAIRWISE_SIGMA_PROPERTY              "sigma"
+#define LJ_SIGMA_PROPERTY                    "sigma"
 
 #define COULOMB_CLASSNAME                    "CoulombPotential"
 #define HERTZIAN_CLASSNAME                   "HertzianPotential"
+#define LENNARDJONES_CLASSNAME               "LJPotential"
 
 #define PAIRWISE_VALUE_METHOD                "value"
 #define PAIRWISE_DERIVATIVE_METHOD           "derivative"
 
 #define PAIRWISE_PRP                         "PrwsPrp"
 #define PAIRWISE_PRP_MSG                     "Pairwise properties."
+
+void functional_vecsub_periodic(unsigned int n, double *a, double *b, double box, double *out);
 
 void pairwise_initialize(void);
