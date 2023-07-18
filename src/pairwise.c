5e2615fabdd6fbf617df49772d8d47ea8eff2fd3
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2023-07-18 14:16:09 -0400

    Fix gravitational potential

--- src/pairwise.c
+++ src/pairwise.c
@@ -30,7 +30,7 @@ value Gravity_value(vm *v, int nargs, value *args) {
     if (nargs==1) {
         double r;
         if (morpho_valuetofloat(MORPHO_GETARG(args, 0), &r)) {
-            out = MORPHO_FLOAT(-1/(r*r));
+            out = MORPHO_FLOAT(-1/r));
         } 
     }
     return out; 
@@ -41,7 +41,7 @@ value Gravity_deriv(vm *v, int nargs, value *args) {
     if (nargs==1) {
         double r;
         if (morpho_valuetofloat(MORPHO_GETARG(args, 0), &r)) {
-            out = MORPHO_FLOAT(2/(r*r*r));
+            out = MORPHO_FLOAT(2/(r*r));
         } 
     }
     return out; 
